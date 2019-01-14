f86821a052bb8d8b34711e24230a4f514521e64a
Masavi <masavi2008@hotmail.com>
2019-01-14 10:53:54 -0600

    add ejercicios backend population

---
+++ 8.1_backend_population/ejercicios.js
@@ -0,0 +1,10 @@
+/*
+----------------------------------------------------------------
+                      Mongo Population
+
+    Resuelve el punto 3) del archivo de ejercicios 
+    de la 'carpeta 8_backend_mongo'
+    
+        https://mongoosejs.com/docs/populate.html
+----------------------------------------------------------------
+*/
